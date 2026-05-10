fec44d50e622f5ab1cfd73a2446453694070e00d
Abdul Kadir <abdulkadir112k@gmail.com>
2026-05-10 23:05:37 +0600

    kadir tisha

--- app/funds/page.jsx
+++ app/funds/page.jsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useStore } from '../../lib/store';
+import { useStore } from '../lib/store';
 import { useEffect, useMemo } from 'react';
 import { format } from 'date-fns';
 import { bn } from 'date-fns/locale/bn';
