74be8d6a6bf172ae3bb546ea53f69c33100a2e4c
Abdul Kadir <abdulkadir112k@gmail.com>
2026-05-10 17:24:34 +0600

    add mosjid fund

--- next.config.js
+++ next.config.js
@@ -4,11 +4,14 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   register: true,
   skipWaiting: true,
-  disable: process.env.NODE_ENV === "development", // dev এ off
+  disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig = {
   reactStrictMode: true,
+
+  // Turbopack config add
+  turbopack: {},
 
   images: {
     remotePatterns: [
