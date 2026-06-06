47c5030a841d050658bdb417818b32fb3f0fa68d
Abdul Kadir <abdulkadir112k@gmail.com>
2026-06-06 08:41:26 +0600

    llkgd

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata, Viewport } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import AuthProvider from "./components/AuthProvider";
-import InstallPWA from "./components/InstallPWA";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -37,7 +36,6 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <AuthProvider>
           {children}
-          <InstallPWA /> {/* ✅ safe */}
         </AuthProvider>
       </body>
     </html>
